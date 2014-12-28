5ed742792b0057b53a1ce8e8ce0bbec5fed1a169
William M <william.r.moy@gmail.com>
2014-12-28 00:10:38 -0800

    Add missing files

---
+++ DownloaderService/HttpClientWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Web.Http;
+
+namespace DownloaderService
+{
+    public class HttpClientWrapper : IHttpClientWrapper
+    {
+        private HttpClient httpClient = new HttpClient();
+
+        public IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> GetAsync(Uri uri)
+        {
+            return this.httpClient.GetAsync(uri);
+        }
+    }
+}
---
+++ DownloaderService/IHttpClientWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Web.Http;
+
+namespace DownloaderService
+{
+    public interface IHttpClientWrapper
+    {
+        IAsyncOperationWithProgress<HttpResponseMessage, HttpProgress> GetAsync(Uri uri);
+    }
+}
