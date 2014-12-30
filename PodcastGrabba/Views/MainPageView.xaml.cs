8f8e15f6ee2aaaed34108716027af90074290bc0
William M <william.r.moy@gmail.com>
2014-12-29 23:56:26 -0800

    namespace cleanup

--- PodcastGrabba/Views/MainPageView.xaml.cs
+++ PodcastGrabba/Views/MainPageView.xaml.cs
@@ -2,22 +2,7 @@
 using Microsoft.Practices.Prism.StoreApps;
 using Microsoft.Practices.Unity;
 using PodcastGrabba.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
 
 namespace PodcastGrabba.Views
 {
